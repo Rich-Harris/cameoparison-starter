4c845971016b9b4f65d3aab0247e52bddbf8254e
Richard Harris <richard.a.harris@gmail.com>
2020-06-11 15:56:45 -0400

    make price visible

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -2,6 +2,8 @@
 	import { createEventDispatcher } from 'svelte';
 
 	export let celeb;
+	export let showprice;
+	export let winner;
 
 	const dispatch = createEventDispatcher();
 </script>
@@ -17,6 +19,12 @@
 
 			<p class="type">{celeb.type}</p>
 		</div>
+
+		{#if showprice}
+			<div class="price" class:large={winner}>
+				<span>${celeb.price}</span>
+			</div>
+		{/if}
 	</button>
 </div>
 
@@ -58,6 +66,25 @@
 		white-space: nowrap;
 		overflow: hidden;
 		text-overflow: ellipsis;
+	}
+
+	.price {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		background-color: rgba(0,0,0,0.3);
+		color: white;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 4em;
+		font-weight: 700;
+	}
+
+	.price.large {
+		font-size: 6em;
 	}
 
 	@media (min-width: 640px) {
